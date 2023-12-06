bdbade3b205803f528c3e17066d3d18add0e5886
To old for code <nengkya@yahoo.com>
2023-12-07 01:45:21 +0700

    StringBuilder is faster than String;
    StringBuilder is faster when performing several concatenations, as it doesn't create a new object each time.

---
+++ StringBuilderExample.java
@@ -0,0 +1,11 @@
+class StringBuilderExample {
+
+    public static void main(String[] arguments) {
+
+	StringBuilder stringBuilder = new StringBuilder("Welcome ");
+	stringBuilder.append("to Hanto GobloG !!!");
+	System.out.println(stringBuilder);
+
+    }
+
+}
