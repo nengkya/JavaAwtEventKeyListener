0c87f72530f49f36574b20a4503e6deaf62f1fac
To old for code <nengkya@yahoo.com>
2023-12-09 01:12:25 +0700

    ArrayList<String>( ); can be resizable, while array cant.

---
+++ ArrayListExample.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class ArrayListExample {
+
+    public static void main(String... arguments) {
+
+	ArrayList<String> cars = new ArrayList<String>();
+
+	cars.add("Volvo");
+	cars.add("BMW");
+
+	System.out.println(cars);
+
+    }
+
+}
